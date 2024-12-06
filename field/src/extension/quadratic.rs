205e7a39acfb1272d6f3050fa057399ae5d2aba8
Milos Djurica <milos.djurica@mvpworkshop.co>
2024-12-06 12:59:43 +0100

    stable changes

--- field/src/extension/quadratic.rs
+++ field/src/extension/quadratic.rs
@@ -181,7 +181,7 @@ impl<F: Extendable<2>> Mul for QuadraticExtension<F> {
     type Output = Self;
 
     #[inline]
-    default fn mul(self, rhs: Self) -> Self {
+    fn mul(self, rhs: Self) -> Self {
         let Self([a0, a1]) = self;
         let Self([b0, b1]) = rhs;
 
@@ -196,21 +196,6 @@ impl<F: Extendable<2>> MulAssign for QuadraticExtension<F> {
     #[inline]
     fn mul_assign(&mut self, rhs: Self) {
         *self = *self * rhs;
-    }
-}
-
-impl<F: Extendable<2>> Square for QuadraticExtension<F> {
-    #[inline(always)]
-    fn square(&self) -> Self {
-        // Specialising mul reduces the computation of c1 from 2 muls
-        // and one add to one mul and a shift
-
-        let Self([a0, a1]) = *self;
-
-        let c0 = a0.square() + <Self as OEF<2>>::W * a1.square();
-        let c1 = a0 * a1.double();
-
-        Self([c0, c1])
     }
 }
 
